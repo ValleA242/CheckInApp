a519b2b21f092d1b28b2d070fde749414635e223
Alicio Valle <alicio.valle242@gmail.com>
2024-05-08 16:53:17 -0400

    Refactored down to one card component utilizing props
    - Removed individual card props and just made a single mood card. Utilized props to differentiate static info between each card. Developed Map functions to populate cards based on number of objects in cards array. Developed map function to list feelings for each card on the mood card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
-
+import MoodCard from "./MoodCard";
 import { CheckInData } from "./CheckInData";
-import { GreenMoodCard } from "./moodcards/GreenMoodCard";
-import { BlueMoodCard } from "./moodcards/BlueMoodCard";
-import { YellowMoodCard } from "./moodcards/YellowMoodCard";
-import { RedMoodCard } from "./moodcards/RedMoodCard";
 
 function App() {
+  const [cards, setCards] = useState([
+    { title: 'Blue Zone', imagePath: "/images/blue.png", feelings: ['Sad', 'Bored', 'Tired'] },
+    { title: 'Green Zone', imagePath: "/images/green.png", feelings: ['Happy', 'Focused', 'Calm'] },
+    { title: 'Yellow Zone', imagePath: "/images/yellow.png", feelings: ['Excited', 'Energetic', 'Cheerful'] },
+    { title: 'Red Zone', imagePath: "/images/red.png", feelings: ['Angry', 'Frustrated', 'Anxious'] }
+  ]);
+
   return (
     <>
       <div className="formContainer">
         <CheckInData />
       </div>
       <div className="cardsContainer">
-        <GreenMoodCard />
-        <BlueMoodCard />
-        <YellowMoodCard />
-        <RedMoodCard />
+        {cards.map((card, index) => (
+          <MoodCard key={index} title={card.title} imagePath={card.imagePath} feelings={card.feelings} />
+        ))}
       </div>
     </>
   );
---
+++ src/MoodCard.jsx
@@ -0,0 +1,13 @@
+const MoodCard = ({ title, imagePath, feelings }) => {
+    return (
+        <div className="primaryContainer">
+            <h1>{title}</h1>
+            <img src={imagePath} alt={`Picture representing ${title}`} />
+            <ul>
+                {feelings.map((feeling, index) => (
+                    <li key={index}>{feeling}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
